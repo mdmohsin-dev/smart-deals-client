eca8bb499e0772340075f044928f72fcdd3b6c65
md mohosin <mdmohosin.web@gmail.com>
2026-06-22 16:20:01 +0600

    chore: update project configuration

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDz9A2vmeSAtmZBnHg6zKNKyW1qoD_J8W0",
-  authDomain: "smart-deals-3a6c1.firebaseapp.com",
-  projectId: "smart-deals-3a6c1",
-  storageBucket: "smart-deals-3a6c1.firebasestorage.app",
-  messagingSenderId: "202166043666",
-  appId: "1:202166043666:web:8828f0557891796bb684cb"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
